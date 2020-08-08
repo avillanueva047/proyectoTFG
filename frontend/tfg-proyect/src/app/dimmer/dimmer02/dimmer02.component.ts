eac8eb36fa59aad282a8a65fbcf32499b2233b36
Adalid Villanueva <avillanueva047@gmail.com>
2020-08-08 17:28:06 +0200

    dimmer 02 animation ended

--- frontend/tfg-proyect/src/app/dimmer/dimmer02/dimmer02.component.ts
+++ frontend/tfg-proyect/src/app/dimmer/dimmer02/dimmer02.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Dimmer02Component implements OnInit {
 
+  degree : number = 0;
+  state : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loop();
   }
 
+  loop(){
+    let regulator = document.getElementById("Regulator");
+    var minReached = true;
+    setInterval(
+      () => {
+        if(!this.state){
+          this.state = true;
+          this.degree = 10;
+        }
+
+        if(minReached){
+          if (this.degree > 260){
+            minReached = false;
+          }
+          this.degree = this.degree + 2.6;
+        }
+        else{
+          if (this.degree < 10){
+            minReached = true;
+            this.degree = 0;
+            this.state = false;
+          }
+          this.degree = this.degree - 2.6;
+        }
+
+        regulator.style.setProperty("transform", `rotate(${this.degree}deg)`);
+      }, 100
+    );
+  }
 }

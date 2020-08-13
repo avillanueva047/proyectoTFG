068dc5dbdab5c4b188c5365ac5b33394a3b9484f
Adalid Villanueva <avillanueva047@gmail.com>
2020-08-13 12:59:30 +0200

    capacity ended

--- frontend/tfg-proyect/src/app/capacity/capacity01/capacity01.component.ts
+++ frontend/tfg-proyect/src/app/capacity/capacity01/capacity01.component.ts
@@ -14,19 +14,54 @@ export class Capacity01Component implements OnInit {
   currCapacity: number;
 
   ngOnInit(): void {
-    this.maxCapacity = 9999;
+    this.maxCapacity = 999;
     this.currCapacity = 0;
     this.loop();
   }
 
   loop(){
+    let title = document.getElementById("title");
     let state = document.getElementById("state");
     let slash = document.getElementById("slash");
     let current = document.getElementById("Current");
     let limit = document.getElementById("Limit");
     setInterval(
       () => {
+
+        //Centering text
+        switch(this.currCapacity.toString().length){
+          case(1):
+            current.setAttribute("transform", "matrix(1 0 0 1 320 745)");
+          break;
+          case(2):
+            current.setAttribute("transform", "matrix(1 0 0 1 260 745)");
+          break;
+          case(3):
+            current.setAttribute("transform", "matrix(1 0 0 1 200 745)");
+          break;
+          case(4):
+            current.setAttribute("transform", "matrix(1 0 0 1 120 745)");
+          break;
+        }
+
+        switch(this.maxCapacity.toString().length){
+          case(1):
+            limit.setAttribute("transform", "matrix(1 0 0 1 780 745)");
+          break;
+          case(2):
+            limit.setAttribute("transform", "matrix(1 0 0 1 730 745)");
+          break;
+          case(3):
+            limit.setAttribute("transform", "matrix(1 0 0 1 670 745)");
+          break;
+          case(4):
+            limit.setAttribute("transform", "matrix(1 0 0 1 630 745)");
+          break;
+        }
+
+        //Animation
         if(this.currCapacity == this.maxCapacity){
+          title.style.setProperty("fill", "red");
           current.style.setProperty("fill", "red");
           slash.style.setProperty("fill", "red");
           limit.style.setProperty("fill", "red");
